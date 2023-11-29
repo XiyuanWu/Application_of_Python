6879d58825db898da2d70d762180fe41c8fdd30d
Xiyuan Wu <149738743+XiyuanWu@users.noreply.github.com>
2023-11-29 14:35:12 -0800

    a

--- Data_Analysis/Projects1.py
+++ Data_Analysis/Projects1.py
@@ -1,77 +1,35 @@
-'''一、 读取文件并划分数据集'''
-# 导入pandas模块，简写成pd
 import pandas as pd
-# 导入matplotlib库中的pyplot包，简写成plt
 import matplotlib.pyplot as plt
 
-# 读取路径为/Users/forecast/jetrail.csv的文件，赋值给变量df
 df = pd.read_csv("/Users/forecast/jetrail.csv")
 
-# 使用to_datetime函数将df中Datetime列数据转化为时间格式，赋值给df["Datetime"]
 df["Datetime"] = pd.to_datetime(df["Datetime"]) 
-# 使用set_index函数将Datetime列设为行索引，并对原DataFrame df生效
-df.set_index(["Datetime"],inplace=True)
+df.set_index(["Datetime"], inplace=True)
 
-# 划分训练集和测试集
-# df中10000-17552行数据为训练集，赋值给变量train
 train = df.iloc[10000:17553]
-# df中17553-最后一行数据为测试集，赋值给变量test
 test = df.iloc[17553:]
 
-# 使用resample()函数按天聚合数据，并通过.mean()函数计算均值，赋值给变量df
 df = df.resample("D").mean()
-# 使用resample()函数按天聚合数据，并通过.mean()函数计算均值，赋值给变量train
 train = train.resample("D").mean()
-# 使用resample()函数按天聚合数据，并通过.mean()函数计算均值，赋值给变量test
 test = test.resample("D").mean()
 
-# 使用copy()函数，将test数据复制给变量y_hat_avg
 y_hat_avg = test.copy()
 
-'''二、 直接均值法'''
-# 使用mean()函数计算train中Count列的均值，赋值给y_hat_avg["avg_forecast"]
 y_hat_avg["avg_forecast"] = train["Count"].mean()
 
-'''三、 移动均值法（10日移动均值）'''
-# 将train["Count"].copy()赋值给新变量train_series
 train_series = train["Count"].copy()
 
-# 定义一个空列表result，用于存储预测结果
 result = []
 
-# for循环遍历y_hat_avg.index
 for i in y_hat_avg.index:
-    
-    # 用rolling()函数求train_series的移动平均值，聚合函数用mean()，将移动平均值的最后一个值赋值给变量
     predict = train_series.rolling(window=10).mean()[-1]
-    
-    # 将predict赋值给train_series[i]
     train_series[i] = predict
-    
-    # 使用append()函数，将predict添加到result中
     result.append(predict)
-    
-'''四、结果可视化'''
-# 使用plot()函数画出训练集折线图
-# X轴数据为train.index，Y轴数据为train["Count"]，颜色为blue，label为train
-plt.plot(train.index, train["Count"],color="blue",label="train")
 
-# 使用plot()函数画出测试集折线图
-# X轴数据为test.index，Y轴数据为test["Count"]，颜色为orange，label为test
-plt.plot(test.index, test["Count"],color="gray",label="test")
+plt.plot(train.index, train["Count"], color="blue", label="train")
+plt.plot(test.index, test["Count"], color="gray", label="test")
+plt.plot(y_hat_avg.index, y_hat_avg["avg_forecast"], color="red", label="Average Forecast")
+plt.plot(y_hat_avg.index, result, color="green", label="Moving Average Forecast")
 
-# 使用plot()函数画出直接均值法预测折线图
-# 轴数据为y_hat_avg.index，Y轴数据为y_hat_avg["avg_forecast"]
-# 颜色为red，label为Average Forecast
-plt.plot(y_hat_avg.index,y_hat_avg["avg_forecast"],color="red",label="Average Forecast")
-
-# 使用plot()函数画出移动均值法预测折线图
-# X轴数据为y_hat_avg.index，Y轴数据为y_hat_avg["moving_avg_forecast"]
-# 颜色为green，label为Moving Average Forecast
-plt.plot(y_hat_avg.index,result,color="green",label="Moving Average Forecast")
-
-# 使用legend()函数显示图例
 plt.legend()
-
-# 使用show()函数显示图片
 plt.show()
--- Data_Analysis/Projects2.py
+++ Data_Analysis/Projects2.py
@@ -1,93 +1,48 @@
-# 导入pandas模块
 import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import roc_auc_score
 
-# 读取数据
-df=pd.read_csv("/Users/data5_1/train_data.csv")
+df = pd.read_csv("/Users/data5_1/train_data.csv")
 
-# 使用replace()函数将grade列的数据的A、B、C、D、E、F、G替换为7、6、5、4、3、2、1
-df["grade"] = df["grade"].replace({'A': 7, 'B': 6, 'C': 5,'D': 4, 'E': 3, 'F': 2, 'G': 1})
+df["grade"] = df["grade"].replace({'A': 7, 'B': 6, 'C': 5, 'D': 4, 'E': 3, 'F': 2, 'G': 1})
 
-# 将debt列和loan列的数据用0填充
 df["debt"] = df["debt"].fillna(0)
 df["loan"] = df["loan"].fillna(0)
 
-"""回归预测缺失值"""
+lr_data = df[["code", "investment", "stock", "income", "expenditure_1", "expenditure_2", "debt", "loan", "grade", "Label"]]
 
-# 从sklearn.linear_model模块导入LinearRegression函数
-from sklearn.linear_model import LinearRegression
-
-# 将不含有缺失值的"code","investment","stock","income","expenditure_1","expenditure_2","debt","loan","grade","Label"列数据单独取出，赋值为lr_data
-lr_data = df[["code","investment","stock","income","expenditure_1","expenditure_2","debt","loan","grade","Label"]]
-
-# 循环遍历每个有缺失值的列名
-for i in ["capital","assets_1","assets_2","expenditure_3","tax"]:
-    
-    # 将i列的数据拷贝出来赋值给dfi变量
+for i in ["capital", "assets_1", "assets_2", "expenditure_3", "tax"]:
     dfi = df[i].copy()
-    # 用isnull()函数判断dfi中的缺失情况，为一个布尔序列，赋值给null_index
     null_index = dfi.isnull()
-    # 用~null_index索引lr_data中不含有i列缺失值的行，做为回归的训练集的自变量x
-    lr_train= lr_data[~null_index]
-    # 用null_index索引lr_data中含缺失值的行，做为回归的测试集的自变量x
+    lr_train = lr_data[~null_index]
     lr_test = lr_data[null_index]
-    # 用~null_index索引df中不含缺失值的行，然后取出它们i列的数据，做为回归的训练集的因变量y
     lr_y_train = df[~null_index][i]
-    # 使用LinearRegression()初始化模型，赋值给lr_model
     lr_model = LinearRegression()
-    # 使用lr_model模型的fit()函数，输入此时训练集的x和y训练模型
-    lr_model.fit(lr_train,lr_y_train)
-    # 用lr_model模型的predict()对测试集的自变量lr_test做预测，也就是预测缺失值的值，赋值为predict
+    lr_model.fit(lr_train, lr_y_train)
     predict = lr_model.predict(lr_test)
-    # 循环遍历测试集的长度
     for j in range(len(lr_test)):
-        # 将测试集索引的第j个值取出赋值为index
         index = lr_test.index[j]
-        # 将dfi中index出的值设置为predict中的第j个值，此步即是在用回归预测值填补缺失值
         dfi[index] = predict[j]
-    # 最后将处理完的第i列数据dfi重新赋值给df[i]列，用填充完缺失值的列覆盖原列
     df[i] = dfi
 
-
-"""决策树分类"""
-
-# 将df中的ID列和Label列用drop()删去做为决策树分类的x
-x = df.drop(columns=["ID","Label"]) 
-# 将df中的Label列做为决策树分类的y
+x = df.drop(columns=["ID", "Label"])
 y = df["Label"]
 
-# 导入sklearn.model_selection模块中的train_test_split函数
-from sklearn.model_selection import train_test_split
-# 使用train_test_split()函数划分训练集和测试集，test_size=0.2,random_state=51
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=51)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=51)
 
-# 导入sklearn.tree模块中的分类决策树模型DecisionTreeClassifier
-from sklearn.tree import DecisionTreeClassifier
-# 使用DecisionTreeClassifier()初始化模型
-# 并设置参数max_depth=3,random_state=598，赋值给model
-DT_model = DecisionTreeClassifier(max_depth=3,random_state=598)
+DT_model = DecisionTreeClassifier(max_depth=3, random_state=598)
 
-# 导入sklearn.model_selection模块中的交叉验证的函数cross_val_score
-from sklearn.model_selection import cross_val_score
+cross_val_auc = cross_val_score(DT_model, x_train, y_train, scoring='roc_auc', cv=10)
 
-# 使用cross_val_score()函数进行交叉验证，将结果赋值给auc_score，scoring设置为'roc_auc',cv设置为10
-cross_val_auc = cross_val_score(DT_model, x_train, y_train, scoring = 'roc_auc',cv=10)
-
-# 输出cross_val_auc的均值
 print(cross_val_auc.mean())
 
-# 用训练集拟合DT_model
 DT_model.fit(x_train, y_train)
 
-# 对测试集的x做概率预测
 y_pred_proba = DT_model.predict_proba(x_test)
 
-"""输出AUC"""
+auc = roc_auc_score(y_test, y_pred_proba[:, 1])
 
-# 导入sklearn.metrics模块中的roc_auc_score函数
-from sklearn.metrics import roc_auc_score
-
-# 将y_test和预测的概率传入roc_auc_score()，将结果赋值给auc
-auc = roc_auc_score(y_test, y_pred_proba[:,1])
-
-# 输出auc
 print(auc)
