f6f80f04ecb5a8f95aa038b17525e11992a7ae6a
Xiyuan Wu <149738743+XiyuanWu@users.noreply.github.com>
2023-11-30 21:32:20 -0800

    Make some changes

--- Web_Crawler/Project.py
+++ Web_Crawler/Project.py
@@ -4,45 +4,43 @@ import time
 import pandas as pd
 import random
 
-headers = {"User-Agent": "Mozilla/5.0"}
+headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"}
 
-total_price_list = []
-price_list = []
-area_list = []
-name_list = []
-areaName_list = []
-room_list = []
+titles = []
+position_icons = []
+house_icons = []
+price_infos = []
+total_prices = []
+unit_price = []
 
 for page in range(1, 6):
-    url = f"https://cd.ke.com/ershoufang/pg{page}su1ie2sf1l2p5/"
-    res = requests.get(url, headers=headers)
-    html = res.text
-    soup = BeautifulSoup(html, "lxml")
-    content_all = soup.find_all(class_="VIEWDATA CLICKDATA maidian-detail")
+    url = f"https://nocturne-spider.baicizhan.com/practise/61/PAGE/{page}.html"
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        continue
+    soup = BeautifulSoup(response.text, 'lxml')
+    content_all = soup.find_all(class_="info clear")
 
     for content in content_all:
-        detail_url = content.attrs["href"]
-        detail_res = requests.get(detail_url, headers=headers)
-        detail_html = detail_res.text
-        detail_soup = BeautifulSoup(detail_html, "lxml")
-        total_price = detail_soup.find(class_="total").string
-        total_price_list.append(total_price)
-        price = detail_soup.find(class_="unitPriceValue").string
-        price_list.append(price)
-        area = detail_soup.find(class_="area").contents[1].string
-        area_list.append(area)
-        name = detail_soup.find(class_="communityName").contents[3].string
-        name_list.append(name)
-        areaName = detail_soup.find(class_="areaName").find(class_="info").contents[1].string
-        areaName_list.append(areaName)
-        room = detail_soup.find(class_="base").find("li").contents[1]
-        room_list.append(room)
-        print(f"{total_price},{price},{area},{name},{areaName},{room}")
-        time.sleep(random.randint(1, 3))
+        title = content.find(class_="title").contents[1].string
+        titles.append(title)
+        position = content.find(class_="positionInfo").a.string
+        position_icons.append(position)
+        house = content.find(class_="houseInfo").contents[2].replace('\n', '').replace('\r', '')
+        house_icons.append(house)
+        totalPrice = content.find(class_="totalPrice totalPrice2").text.replace('\n', '').replace('\r', '')
+        total_prices.append(totalPrice)
+        unitPrice = content.find(class_="unitPrice").span.string
+        unit_price.append(unitPrice)
 
-total = {"房子总价/万": total_price_list, "单价价格(元/平方)": price_list, "建筑面积": area_list, "小区名称": name_list, "所在区域": areaName_list, "房屋户型": room_list}
+        print(f"{title},{position},{house},{totalPrice},{unitPrice}")
+
+total = {"房屋户型": titles, "小区地址": position_icons, "建筑信息": house_icons, "单价价格(元/平方)": unit_price,"房子总价/万": total_prices}
+
 info = pd.DataFrame(total)
-writer = pd.ExcelWriter("二手房.xlsx")
+
+writer = pd.ExcelWriter("/Users/二手房.xlsx")
+
 info.to_excel(excel_writer=writer, sheet_name="成都")
+
 writer.save()
-writer.close()
--- Web_Crawler/README.md
+++ Web_Crawler/README.md
@@ -1,1 +1,13 @@
-#r
+# Web Crawler Projects
+
+## Project Overview
+
+### Understanding the Purpose
+Yoyo recently has some spare funds and is looking to invest in a second-hand house priced between 1 to 1.5 million. She aims to acquire comprehensive information about available properties in Chengdu to make a well-informed decision. However, gathering the necessary data for purchasing a house can be overwhelming. How can one quickly obtain property listings? She plans to write a program that retrieves information from the first 5 pages of a specific website and records it in an Excel file. Listings that are no longer available will be excluded from the data collection.
+
+## Results
+<img src="./tu.png" alt="Web Crawler Project Results" style="width: 1000px;"/>
+
+
+*Note: Course is taken Non-English so output result is also Non-English.*
+
